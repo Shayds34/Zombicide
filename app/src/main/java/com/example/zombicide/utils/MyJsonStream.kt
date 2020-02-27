e52eb5cb6a3489ae3369ca81492db9f800530891
Shayds <shayds@hotmail.fr>
2020-02-27 14:25:53 +0100

    Enhancement

--- app/src/main/java/com/example/zombicide/utils/MyJsonStream.kt
+++ app/src/main/java/com/example/zombicide/utils/MyJsonStream.kt
@@ -1,12 +1,14 @@
 package com.example.zombicide.utils
 
 import android.content.Context
+import android.util.Log
+import com.example.zombicide.models.CustomSkill
 import com.example.zombicide.models.Hero
 import com.example.zombicide.models.Skill
-import com.example.zombicide.models.Skills
 import com.google.gson.Gson
 
 class MyJsonStream {
+
     companion object{
         const val TAG = "MyJsonStream"
     }
@@ -16,6 +18,7 @@ class MyJsonStream {
     private val mGson = Gson()
 
     fun getHeroes(context: Context) : List<Hero> {
+        Log.d(TAG, "Getting heroes list.")
         val mInputStream = context.assets.open("heroes.json")
         mHeroesJson = mInputStream.bufferedReader().use{ it.readText()}
 
@@ -23,9 +26,25 @@ class MyJsonStream {
     }
 
     fun getSkills(context: Context) : List<Skill> {
+        Log.d(TAG, "Getting skills list.")
         val mInputStream = context.assets.open("skills.json")
         mSkillsJson = mInputStream.bufferedReader().use{ it.readText()}
 
         return (mGson.fromJson(mSkillsJson, Array<Skill>::class.java)).toList()
+    }
+
+    fun createCustomSkillsList(context: Context) {
+        Log.d(TAG, "Creating custom skills list.")
+
+        val data = this.getSkills(context)
+        val mCustomSkills = ArrayList<CustomSkill>()
+
+        for (skill in data) {
+            val mCustomSkill = CustomSkill(skill.skill_id, skill.skill_name)
+            mCustomSkills.add(mCustomSkill)
+        }
+
+        val file = mCustomSkills.joinToString(separator = "\n")
+        Log.d(TAG, file)
     }
 }
